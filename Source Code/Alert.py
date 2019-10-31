0be3245b3e777ca83357c8e7aa5d34ef508d6941
Emily Reinherz <reinherz@amazon.com>
2019-10-31 10:10:57 -0700

    add anomaly bool, up write capacity for patientref

--- Source Code/Alert.py
+++ Source Code/Alert.py
@@ -14,27 +14,33 @@ lower_threshold = float(os.environ['LOWER_THRESHOLD'])
 ENCODING = 'utf-8'
 def lambda_handler(event, context):
     output_records = []
+    
     for record in event['records']:
         data = b64decode(record['data']).decode(ENCODING)
-        add_newline = data + "\n"
-        output_data = b64encode(add_newline.encode(ENCODING)).decode(ENCODING)
-        
-        output_records.append({
-            'recordId': record['recordId'],
-            'result': 'Ok',
-            'data': output_data
-        })
-        
         data_json = json.loads(data)
         print("Data: ",json.dumps(data_json))
         anomaly_score = data_json['ANOMALY_SCORE']
+
+        anomaly_found = False
         
         if anomaly_score > upper_threshold:
-            print("Anomaly Found!")
+            anomaly_found = True
             response = sns.publish(TopicArn=topic_arn, Message=data, Subject='Anomaly Score Above ' + str(upper_threshold))
             print('SNS Publish: ',json.dumps(response))
         elif anomaly_score < lower_threshold:
-            print("Anomaly Found!")
+            anomaly_found = True
             response = sns.publish(TopicArn=topic_arn, Message=data, Subject='Anomaly Score Below ' + str(lower_threshold))
             print('SNS Publish: ',json.dumps(response))
+
+        data_json['anomaly'] = anomaly_found
+
+        add_newline = json.dumps(data_json) + "\n"
+        output = b64encode(add_newline.encode(ENCODING)).decode(ENCODING)
+
+        output_records.append({
+            'recordId': record['recordId'],
+            'result': 'Ok',
+            'data': output
+        })
+
     return {'records': output_records}
